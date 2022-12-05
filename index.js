536579f94725cc2cf90e3020cd6d332c9c058c31
HogmanGlenn <ghogman19@student.oulu.fi>
2022-12-05 18:17:13 +0200

    Website creation day 2
    Colors changed
    Pictures changed
    Added languages
    Added up-down arrows + hover
    Changed links

---
+++ index.js
@@ -0,0 +1,9 @@
+const upbtn = document.getElementById("upButton");
+const dwnbtn = document.getElementById("downButton");
+
+upbtn.addEventListener("click", function handleClick(){
+    btn.style.backgroundColor = transparent;
+    btn.textContent = "LinkedIn";
+});
+
+dwnbtn.addEventListener("click", function handleClick(){
