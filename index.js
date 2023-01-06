36999c5c90600483942730df1a2d04e102a2d9eb
HogmanGlenn <ghogman19@student.oulu.fi>
2023-01-06 18:00:53 +0200

    Features added
    +Twitter link
    +LinkedIn link
    +CV embedded and linked
    +Home now takes you to main page
    +Added ability to view CV on page
    +Button to leave CV view

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const upbtn = document.getElementById("upButton");
-const dwnbtn = document.getElementById("downButton");
 
-upbtn.addEventListener("click", function handleClick(){
-    btn.style.backgroundColor = transparent;
-    btn.textContent = "LinkedIn";
-});
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+}
 
-dwnbtn.addEventListener("click", function handleClick(){
+function ShowCV(){
+    var x = document.getElementById('CV');
+    if(x.style.display =='none'){
+        x.style.display ='block';
+    } else{
+        x.style.display ='none';
+    }
+}
